16779832e6326a0c37bc281bf723d35bb67064f0
trainer14 <kevin.clark@dish.com>
2017-04-23 14:19:40 -0600

    Added config test

---
+++ src/test/java/com/example/service/WordConfigTest.java
@@ -0,0 +1,36 @@
+package com.example.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Kevin Clark.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = {
+        "wordCount.caseSensitive=false",
+        "wordCount.words.skip[0]=apple",
+        "wordCount.words.skip[1]=orange",
+        "wordCount.words.skip[2]=Arya",
+})
+public class WordConfigTest {
+    @Autowired
+    private WordConfig wordConfig;
+
+    @Test
+    public void wordConfigTest() {
+        assert ( wordConfig.getWords().getSkip().contains( "apple" ));
+        assert ( wordConfig.getWords().getSkip().contains( "orange" ));
+        assert ( wordConfig.getWords().getSkip().contains( "Arya" ));
+        assert ( !wordConfig.isCaseSensitive());
+        
+    }
+}
+
+
+
